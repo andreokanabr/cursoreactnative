ad0ef55ba698280c58e148fbe592c02ee876c0a4
andre okanabr <alhkanashiro@gmail.com>
2026-02-25 17:43:07 -0300

    Add Login to Home

--- Fin_ancas/src/routes/app.routes.js
+++ Fin_ancas/src/routes/app.routes.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useWindowDimensions } from 'react-native';
 
 import Home from '../pages/Home';
 
 const AppDrawer = createDrawerNavigator();
 
-function AppRoutes(){
-  return(
-    <AppDrawer.Navigator>
-      <AppDrawer.Screen
-        name="Home"
-        component={Home}
-      />
+function AppRoutes() {
+  const dimensions = useWindowDimensions();
+
+  return (
+    <AppDrawer.Navigator
+      screenOptions={{
+        drawerType: dimensions.width >= 768 ? 'permanent' : 'front',
+      }}
+    >
+      <AppDrawer.Screen name="Home" component={Home} />
     </AppDrawer.Navigator>
-  )
+  );
 }
 
 export default AppRoutes;
