892628c6933ea344a7849fed94d3bea7969af1c2
andre okanabr <alhkanashiro@gmail.com>
2026-03-09 11:53:05 -0300

    UPDATE notificaApp

--- notificaApp/App.tsx
+++ notificaApp/App.tsx
@@ -1,37 +1,138 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import notifee, {
+  AuthorizationStatus,
+  EventType,
+  AndroidImportance,
+  TriggerType,
+  TimestampTrigger,
+} from '@notifee/react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [statusNotification, setStatusNotification] = useState(true);
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+  useEffect(() => {
+    async function getPermission() {
+      const settings = await notifee.requestPermission();
+      if (settings.authorizationStatus >= AuthorizationStatus.AUTHORIZED) {
+        console.log('Permission: ', settings.authorizationStatus);
+        setStatusNotification(true);
+      } else {
+        console.log('Usuario negou a permissao!');
+        setStatusNotification(false);
+      }
+    }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+    getPermission();
+  }, []);
+
+  notifee.onBackgroundEvent(async ({ type, detail }) => {
+    const { notification, pressAction } = detail;
+
+    if (type === EventType.PRESS) {
+      console.log('TOCOU NA NOTIFICACAO BACKGROUND: ', pressAction?.id);
+      if (notification?.id) {
+        await notifee.cancelNotification(notification?.id);
+      }
+    }
+
+    console.log('EVENT BACKGROUND');
+  });
+
+  useEffect(() => {
+    return notifee.onForegroundEvent(({ type, detail }) => {
+      switch (type) {
+        case EventType.DISMISSED:
+          console.log('USUARIO DESCARTOU A NOTIFICACAO');
+          break;
+        case EventType.PRESS:
+          console.log('TOCOU: ', detail.notification);
+          console.log('Title ', detail.notification?.title);
+          console.log('Corpo ', detail.notification?.body);
+      }
+    });
+  }, []);
+
+  async function handleNotificate() {
+    if (!statusNotification) {
+      return;
+    }
+
+    const channelId = await notifee.createChannel({
+      id: 'lembrete',
+      name: 'lembrete',
+      vibration: true,
+      importance: AndroidImportance.HIGH,
+    });
+
+    await notifee.displayNotification({
+      id: 'lembrete',
+      title: 'Estudar programaçao!',
+      body: 'Lembrete para estudar react-native amanha!',
+      android: {
+        channelId,
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
+  async function handleScheduleNotification() {
+    const date = new Date(Date.now());
+
+    date.setMinutes(date.getMinutes() + 1);
+
+    const trigger: TimestampTrigger = {
+      type: TriggerType.TIMESTAMP,
+      timestamp: date.getTime(),
+    };
+
+    const notification = await notifee.createTriggerNotification(
+      {
+        title: 'Lembrete Estudo',
+        body: 'Estudar JavaScript as 15:30',
+        android: {
+          channelId: 'lembrete',
+          importance: AndroidImportance.HIGH,
+          pressAction: {
+            id: 'default',
+          },
+        },
+      },
+      trigger,
+    );
+
+    console.log('Notification agendada: ', notification);
+  }
+
+  function handleListNotifications() {
+    notifee.getTriggerNotificationIds().then(ids => {
+      console.log(ids);
+    });
+  }
+
+  async function handleCancelNotification() {
+    // await notifee.cancelNotification('0MiUNtfxHsIZrvJcUJcT');
+    await notifee.cancelAllNotifications();
+
+    console.log('Notificaçao cancelada com sucesso!');
+  }
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+      <Text>Notificaçoes App</Text>
+      <Button title="Enviar notificaçao" onPress={handleNotificate} />
+
+      <Button
+        title="Agendar notificaçao"
+        onPress={handleScheduleNotification}
       />
+
+      <Button title="Listar notificacoes" onPress={handleListNotifications} />
+
+      <Button title="Cancelar Notificaçao" onPress={handleCancelNotification} />
     </View>
   );
 }
@@ -39,7 +140,7 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
-
-export default App;
